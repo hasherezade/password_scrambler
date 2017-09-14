1e573113cdb247c17f79fc7d330051cb6db579a0
evilsocket <evilsocket@gmail.com>
2017-09-14 14:16:50 +0200

    Some refactoring/semplification. Added optional --scramble-func parameter, default to md5.

--- passcrambler.py
+++ passcrambler.py
@@ -11,86 +11,79 @@ from Crypto.Cipher import AES
 ###
 # AES:
 
-BLOCK_SIZE = 16
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+pad = lambda s: s + (AES.block_size - len(s) % AES.block_size) * chr(AES.block_size - len(s) % AES.block_size)
 
-class AESCipher:
-    def __init__( self, seed, key ):
-        self.key = key
-        self.seed = seed
-
-    def encrypt( self, raw ):
-        raw = pad(raw)
-        iv = self.seed[0:AES.block_size]
-        cipher = AES.new( self.key, AES.MODE_CBC, iv )
-        return base64.b64encode( iv + cipher.encrypt( raw ) ) 
-###
-
-def scramble(key):
-    result = hashlib.md5(key).digest()
-    return result
+def aes_encrypt( seed, key, raw ):
+    raw = pad(raw)
+    iv = seed[0:AES.block_size]
+    cipher = AES.new( key, AES.MODE_CBC, iv )
+    return base64.b64encode( iv + cipher.encrypt( raw ) ) 
 
 ###
 
-def get_raw_bytes(filename, offset=0):
-    fo = open(filename,"rb")
-    fo.seek(offset, 0)
-    data = fo.read()
-    fo.close()
-    return data
+def scramble( key, func='md5' ):
+    # Ugly but effective, moreover this is not a webapp so who cares.
+    # Will raise AttributeError if function is not valid ... auto validation FTW!
+    return eval( 'hashlib.%s(key).digest()' % func )
+
+###
 
 def convert_to_charset(password, specialchars):
     output = ""
-    i = 0
+    i      = 0
+    slen  = len(specialchars)
+
     for c in password:
-        if c >= 'A' and c <= 'Z':
+        if c.isalnum():
             output += c
-            continue
-        if c >= 'a' and c <= 'z':
-            output += c
-            continue
-        if c >= '0' and c <= '9':
-            output += c
-            continue
-        output += specialchars[i]
-        i += 1
-        if i == len(specialchars):
-            i = 0
+        else:
+            output += specialchars[i % slen]
+            i += 1
+
     return output        
    
 def main():
-    parser = argparse.ArgumentParser(description="Password scrambler")
-    parser.add_argument('--file', dest="file", default=None, help="File used to initialize generation", required=True)
-    parser.add_argument('--login', dest="login", default=None, help="Login for which you want to use the password", required=True)
-    parser.add_argument('--special', dest="special", default="_&#", help="Whitelist of special characters, i.e: '_&#'")
-    parser.add_argument('--length', dest="length", default=30, help="Length of the password, default=30", type=int)
-    args = parser.parse_args()
+    try:
+        parser = argparse.ArgumentParser(description="Password scrambler")
+        parser.add_argument('--file', dest="file", default=None, help="File used to initialize generation", required=True)
+        parser.add_argument('--login', dest="login", default=None, help="Login for which you want to use the password", required=True)
+        parser.add_argument('--special', dest="special", default="_&#", help="Whitelist of special characters, i.e: '_&#'")
+        parser.add_argument('--length', dest="length", default=30, help="Length of the password, default=30", type=int)
+        parser.add_argument('--scramble-func', dest="func", default='md5', help="Hashing function to use for input data scrambling, default=md5.\nOther functions can be found on hashlib module documentation.")
+        args = parser.parse_args()
 
-    password = getpass.getpass()
-    key = scramble(password)    
-    vec = scramble(args.login)
+        # first thing first, fail if seed file does not exist
+        with open( args.file, 'rb' ) as fd:
+            raw = fd.read()
 
-    raw = get_raw_bytes(args.file)
-    aes = AESCipher(vec, key)
-    aes_out1 = aes.encrypt(raw)
-    del aes
+        password = getpass.getpass()
+        key = scramble( password, args.func )    
+        vec = scramble( args.login, args.func )
 
-    sha_digest = hashlib.sha512(aes_out1).digest()
-    passlen = len(password) % len(sha_digest)
-    key2 = sha_digest[passlen: passlen+32]
-    aes = AESCipher(key, key2)
-    aes_out2 = aes.encrypt(aes_out1)
-    del aes
-    
-    start = ord(key[0]) % len(aes_out2)
-    portion = aes_out2[start:]
-    result = hashlib.sha512(portion).digest()
-    longpass = base64.b64encode(result)
-    longpass = longpass[0:args.length]
-    longpass = convert_to_charset(longpass,  sorted(args.special, reverse=True))
-    print "---"
-    print longpass
-    print "---"
+        aes_out1 = aes_encrypt( vec, key, raw )
+
+        sha_digest = hashlib.sha512(aes_out1).digest()
+        passlen    = len(password) % len(sha_digest)
+        key2       = sha_digest[passlen: passlen+32]
+
+        aes_out2 = aes_encrypt( key, key2, aes_out1 )
+        
+        start    = ord(key[0]) % len(aes_out2)
+        portion  = aes_out2[start:]
+        result   = hashlib.sha512(portion).digest()
+        longpass = base64.b64encode(result)
+        longpass = longpass[0:args.length]
+        longpass = convert_to_charset(longpass,  sorted(args.special, reverse=True))
+
+        print "---"
+        print longpass
+        print "---"
+
+    except AttributeError:
+        print "[ERROR] '%s' is not a valid hashing function." % args.func
+
+    except Exception as e:
+        print "[ERROR] %s" % e
 
 if __name__ == "__main__":
     main()
